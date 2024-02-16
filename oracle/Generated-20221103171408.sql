7e64632f8c5c1e2b05ca2f9facfc2bf2b5cecfd6
20K61A0498 <105557499+20K61A0498@users.noreply.github.com>
2024-02-16 08:41:23 +0530

    Add files via upload

---
+++ oracle/Generated-20221103171408.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  File created - Thursday-November-03-2022   
+--------------------------------------------------------
+@C:\21c\ELECTION.sql
+--@C:\21c\ELECTION_DATA_TABLE.csv
+@C:\21c\VOTING.sql
+@C:\21c\ELECTION_I.sql
+@C:\21c\INDEX_NAME.sql
+@C:\21c\ELECTION_CONSTRAINT.sql
